91fc3cab4807620a9c724c764193dba8b77d11b1
Arwil Tinambacan <arwil.tnmbcn@gmail.com>
2023-04-05 16:14:39 +0800

    child routing to dashboard

--- cmkis/src/app/app-routing.module.ts
+++ cmkis/src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './user-auth/login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './user-auth/auth.guard';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
+
 
 const routes: Routes = [
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard],
+    children: [
+      {path: 'dashboard', component: DashboardComponent},
+    ]
+
+  },
 
 ];
 
