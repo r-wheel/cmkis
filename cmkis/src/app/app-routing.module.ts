6564939ec90c6028a2095353a826030629675dc8
Arwil Tinambacan <arwil.tnmbcn@gmail.com>
2023-04-07 11:50:31 +0800

    home navigation

--- cmkis/src/app/app-routing.module.ts
+++ cmkis/src/app/app-routing.module.ts
@@ -4,7 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './user-auth/login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './user-auth/auth.guard';
+
+
+import { AccountComponent } from './home/account/account.component';
+import { AttendanceCheckerComponent } from './home/attendance-checker/attendance-checker.component';
 import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { FacultyComponent } from './home/faculty/faculty.component';
+import { KeyInventoryComponent } from './home/key-inventory/key-inventory.component';
+import { RoomKeyManagementComponent } from './home/room-key-management/room-key-management.component';
+import { SemesterManagementComponent } from './home/semester-management/semester-management.component';
+import { SettingsComponent } from './home/settings/settings.component';
 
 
 const routes: Routes = [
@@ -13,9 +22,15 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard],
     children: [
+      {path: 'account', component: AccountComponent},
+      {path: 'attendance-checker', component: AttendanceCheckerComponent},
       {path: 'dashboard', component: DashboardComponent},
+      {path: 'faculty', component: FacultyComponent},
+      {path: 'key-inventory', component: KeyInventoryComponent},
+      {path: 'room-key-management', component: RoomKeyManagementComponent},
+      {path: 'semester-management', component: SemesterManagementComponent},
+      {path: 'settings', component: SettingsComponent},
     ]
-
   },
 
 ];
