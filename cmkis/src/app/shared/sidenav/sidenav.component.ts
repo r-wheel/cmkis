e3e4a89465560eb3f24e0d08924d5f260892306f
blssblgts <balagtas.blessie.e@gmail.com>
2023-04-17 21:48:37 +0800

    update

--- cmkis/src/app/shared/sidenav/sidenav.component.ts
+++ cmkis/src/app/shared/sidenav/sidenav.component.ts
@@ -16,15 +16,14 @@ export class SidenavComponent{
 
     closeBtn.addEventListener('click', () => {
       sidebar.classList.toggle('open');
-      menuBtnChange(); //calling the function(optional)
+      menuBtnChange();
     });
 
-    // following are the code to change sidebar button(optional)
     function menuBtnChange() {
       if (sidebar.classList.contains('open')) {
-        closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right'); //replacing the iocns class
+        closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right');
       } else {
-        closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu'); //replacing the iocns class
+        closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu');
       }
     }
   }
