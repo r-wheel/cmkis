a48112b9566bbbfc6c779f5774825e4022af1dfa
blssblgts <balagtas.blessie.e@gmail.com>
2023-04-22 21:05:38 +0800

    nav update

--- cmkis/src/app/shared/sidenav/sidenav.component.ts
+++ cmkis/src/app/shared/sidenav/sidenav.component.ts
@@ -3,29 +3,29 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent{
-  name = 'Angular ' + VERSION.major;
+ // name = 'Angular ' + VERSION.major;
 
   constructor() { }
 
   ngOnInit(): void {
-    let sidebar = document.querySelector('.sidebar');
-    let closeBtn = document.querySelector('#btn');
+    // let sidebar = document.querySelector('.sidebar');
+    // let closeBtn = document.querySelector('#btn');
 
-    closeBtn.addEventListener('click', () => {
-      sidebar.classList.toggle('open');
-      menuBtnChange();
-    });
+    // closeBtn.addEventListener('click', () => {
+    //   sidebar.classList.toggle('open');
+    //   menuBtnChange();
+    // });
 
-    function menuBtnChange() {
-      if (sidebar.classList.contains('open')) {
-        closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right');
-      } else {
-        closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu');
-      }
-    }
+    // function menuBtnChange() {
+    //   if (sidebar.classList.contains('open')) {
+    //     closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right');
+    //   } else {
+    //     closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu');
+    //   }
+    // }
   }
 
 }
