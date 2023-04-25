1668991d87d736ad3b7763d08e4584c305a94f9d
Arwil Tinambacan <arwil.tnmbcn@gmail.com>
2023-04-25 21:54:38 +0800

    A-T checker display table per Day

--- cmkis/src/app/home-at/check-attendance/check-attendance.component.ts
+++ cmkis/src/app/home-at/check-attendance/check-attendance.component.ts
@@ -13,6 +13,7 @@ export class CheckAttendanceComponent implements OnInit {
 
   dateDisplay:Date;
   timeDisplay:Date;
+  data:any;
 
   displayColumns: string[] = [
     'room',
@@ -25,7 +26,7 @@ export class CheckAttendanceComponent implements OnInit {
     'time_end',
   ];
 
-  dataSource: any = [];
+  dataSource: any;
 
   auth:any;
 
@@ -39,19 +40,93 @@ export class CheckAttendanceComponent implements OnInit {
     // }
 
 
-
     timer(0,1000).subscribe(()=>{
       this.dateDisplay = new Date ()
       this.timeDisplay = new Date ()
-      this.getAllSched();
+      // this.getAllSched();
     })
 
-  }
-
-  getAllSched(){
-    return this.httpClient.get('http://localhost/api/schedule_view.php').subscribe((data => {
-      this.dataSource = data;
-    }));
+    this.getAllSched();
 
   }
+
+
+  getAllSched(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.httpClient.get('http://localhost/api/schedule_view.php').subscribe(
+        (data) => {
+          resolve(this.toDisplay(data));
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  }
+
+
+
+  toDisplay(data){
+    console.log(data);
+    if (this.toDay() === 'Monday'){
+      this.getAllMonday(data);
+    }
+    else if (this.toDay() === 'Tuesday') {
+      this.getAllTuesday(data);
+    }
+    else if (this.toDay() === 'Wednesday') {
+      this.getAllTuesday(data);
+    }
+    else if (this.toDay() === 'Thursday') {
+      this.getAllTuesday(data);
+    }
+    else if (this.toDay() === 'Friday') {
+      this.getAllTuesday(data);
+    }
+    else if (this.toDay() === 'Saturday') {
+      this.getAllTuesday(data);
+    }
+  }
+
+
+  toDay(){
+    const today = new Date();
+    const dayOfWeek = today.getDay();
+    const toDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][dayOfWeek]
+    return toDay;
+  }
+
+  
+  getAllMonday(data){
+    const allMonday = data.filter(obj => obj.day === 'Monday');
+    this.dataSource = allMonday;
+  }
+
+  getAllTuesday(data){
+    const allTuesday = data.filter(obj => obj.day === 'Tuesday');
+    this.dataSource = allTuesday;
+  }
+
+  getAllWednesday(data){
+    const allWednesday = data.filter(obj => obj.day === 'Wednesday');
+    this.dataSource = allWednesday;
+  }
+
+  getAllThursday(data){
+    const allThursday = data.filter(obj => obj.day === 'Thursday');
+    this.dataSource = allThursday;
+  }
+
+  getAllFriday(data){
+    const allFriday = data.filter(obj => obj.day === 'Friday');
+    this.dataSource = allFriday;
+  }
+
+  getAllSaturday(data){
+    const allSaturday = data.filter(obj => obj.day === 'Saturday');
+    this.dataSource = allSaturday;
+  }
+
+
+
 }
